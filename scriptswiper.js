9192ab1932e155cb435385c50cda721138aac78b
Abraham <96110567+AbrahamSalguero@users.noreply.github.com>
2023-06-19 13:23:16 +0200

    Update scriptswiper.js

--- scriptswiper.js
+++ scriptswiper.js
@@ -1,27 +1,12 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+const swiper = new Swiper('.swiper', { 
+ // Optional parameters
   direction: 'horizontal',
   loop: true,
   speed: 1000,
-  slidesPerView: 2,
+  slidesPerView: 3,
   autoplay: {
-   delay: 3000,
+   delay: 1000,
  },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    hideOnClick: true,
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
+
+
