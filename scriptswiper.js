6ef41c6adc90f72f01f45f2990ffee5aa8da2ffd
Abraham <96110567+AbrahamSalguero@users.noreply.github.com>
2023-06-27 10:27:26 +0200

    scriptswiper.js

--- scriptswiper.js
+++ scriptswiper.js
@@ -1,16 +1,28 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   speed: 700,
-  effect: 'cards',
-  cardsEffect: {
-    slideShadows:false,
-  },
-   slidesPerView: 1,
+   slidesPerView: 2,
   direction: 'horizontal',
   loop: true,
    autoplay: {
-   delay: 1000,
+   delay: 2000,
  },
+  
+  //effect creative
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      // will set `translateZ(-400px)` on previous slides
+      translate: [0, 0, -600],
+    },
+
+    next: {
+      // will set `translateX(100%)` on next slides
+      translate: ['100%', 0, 0],
+    },
+  },
+  
+ 
 
   // If we need pagination
   pagination: {
